5f98e344a68bbcfc7ce3477c047c00819750395b
Daniel <djp297@cornell.edu>
2019-08-05 20:28:56 -0400

    Add findPreference for orderBy in onCreate in EarthquakePreferenceFragment

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -23,6 +23,9 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         @Override
